980d4648a2abbb0de97f6a90a5c7c7ab3f43d51b
Admin <Admin@DESKTOP-RRH179N>
2021-12-24 01:08:14 +0530

    Draft copy

---
+++ src/test/java/generic/IAutoConst.java
@@ -0,0 +1,8 @@
+package generic;
+
+public interface IAutoConst {
+	
+	
+	String APP_URL="http://www.google.com";
+	String PPT_PATH = "./src/test/resources/data/config.properties";
+}
---
+++ src/test/java/scripts/Demo.java
@@ -0,0 +1,23 @@
+package scripts;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+
+
+
+public class Demo extends BaseTest {
+	@Test
+	public void testA() {
+		
+		Reporter.log("testA", true);
+		
+	
+	}
+
+}

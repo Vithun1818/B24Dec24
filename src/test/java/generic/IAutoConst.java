22ed745dea250b09da92ed997fd5df2b5f2a5967
Admin <Admin@DESKTOP-RRH179N>
2021-12-26 21:46:42 +0530

    Draft copy

--- src/test/java/generic/IAutoConst.java
+++ src/test/java/generic/IAutoConst.java
@@ -2,7 +2,7 @@ package generic;
 
 public interface IAutoConst {
 	
-	
+	String XL_PATH="./src/test/resources/data";
 	String APP_URL="http://www.google.com";
 	String PPT_PATH = "./src/test/resources/data/config.properties";
 }
